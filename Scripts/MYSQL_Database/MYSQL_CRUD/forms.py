b5351ef8684f7250e919c1b71cc3bd7f79543b0c
RanjithaElaiyaperumal <ranjitharanjitha302003@gmail.com>
2025-03-24 10:18:47 +0530

    first commit

---
+++ Scripts/MYSQL_Database/MYSQL_CRUD/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import RegisterForm
+
+
+class MyRegisterForm(forms.ModelForm):
+    class Meta:
+        model=RegisterForm
+        fields=["name","age","address","contact","email"]
